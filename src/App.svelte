ca61e214456818df55b83a228af2bffc21b34d83
ferminrp <frodriguezpenelas@gmail.com>
2021-10-28 19:42:57 -0300

    fixed background

--- src/App.svelte
+++ src/App.svelte
@@ -10,39 +10,43 @@
 </main>
 
 <style>
-	main {
-		background-image: url('/cover.jpg');
-		width: 100vw;
-		height: 100vh;
-		text-align: center;
-		color: white;
-	}
+  main {
+    background-image: url("/cover.jpg");
+    width: 100vw;
+    height: 100vh;
+    text-align: center;
+    color: white;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+    background-size: cover;
+	text-shadow: black 1px 0 10px;
+  }
 
-	h1 {
-		margin: 0px;
-	}
+  h1 {
+    margin: 0px;
+  }
 
-	a {
-		color: white;
-		text-decoration: none;
-		background-color: #3C6E71;
-		width: 200px;
-		margin-left: auto;
-		margin-right: auto;
-		padding: 0.5rem;
-		border-radius: 5px;
-		margin-top: 1rem;
-	}
+  a {
+    color: white;
+    text-decoration: none;
+    background-color: #3c6e71;
+    width: 200px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 0.5rem;
+    border-radius: 5px;
+    margin-top: 1rem;
+  }
 
-	.overlay {
-		background-color: rgba(0, 0, 0, 0.5);
-		width: 100vw;
-		height: 100vh;
-		text-align: center;
-		padding: 1rem;
-		box-sizing: border-box;
-		display: flex;
-		flex-direction: column;
-		justify-content: center
-	}
+  .overlay {
+    background-color: rgba(0, 0, 0, 0.5);
+    width: 100vw;
+    height: 100vh;
+    text-align: center;
+    padding: 1rem;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 </style>
