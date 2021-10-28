2e824aac40b398af984deb8d66f1954c36f811c7
ferminrp <frodriguezpenelas@gmail.com>
2021-10-28 19:35:23 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+</script>
+
+<main>
+  <div class="overlay">
+    <h1>GetRedi</h1>
+    <p>Construimos herramientas para potenciar a los emprendedores</p>
+    <a href="https://wa.me/541134181010">Contactanos</a>
+  </div>
+</main>
+
+<style>
+	main {
+		background-image: url('/cover.jpg');
+		width: 100vw;
+		height: 100vh;
+		text-align: center;
+		color: white;
+	}
+
+	h1 {
+		margin: 0px;
+	}
+
+	a {
+		color: white;
+		text-decoration: none;
+		background-color: #3C6E71;
+		width: 200px;
+		margin-left: auto;
+		margin-right: auto;
+		padding: 0.5rem;
+		border-radius: 5px;
+		margin-top: 1rem;
+	}
+
+	.overlay {
+		background-color: rgba(0, 0, 0, 0.5);
+		width: 100vw;
+		height: 100vh;
+		text-align: center;
+		padding: 1rem;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		justify-content: center
+	}
+</style>
